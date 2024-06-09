5f39c85d87bff1fc910db1bd7444c00cd9be8480
Doru Cheresdi <51168560+DoruCheresdi@users.noreply.github.com>
2024-06-09 19:28:59 +0300

    added org sort and search

--- idp-angular-frontend/src/app/components/organisation-data-view/organisation-data-view.component.ts
+++ idp-angular-frontend/src/app/components/organisation-data-view/organisation-data-view.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges, Output} from '@angular/core';
 import {OrganisationResponse} from "../../dtos/organisation-response";
 import {OrganisationService} from "../../services/organisation.service";
 import {Router} from "@angular/router";
@@ -9,16 +9,27 @@ import {ConfirmationService, MessageService} from "primeng/api";
   templateUrl: './organisation-data-view.component.html',
   styleUrls: ['./organisation-data-view.component.css']
 })
-export class OrganisationDataViewComponent {
+export class OrganisationDataViewComponent implements OnChanges {
 
     @Input() organisations: OrganisationResponse[] = [];
 
+    sortedOrganisations: OrganisationResponse[] = [];
+
     @Input() header = 'Discover Organisations';
+
+    SORT_BY_RATING = 'By Reviews Rating';
+    SORT_BY_DEFAULT = 'Default';
+    sortOptions = [this.SORT_BY_RATING, this.SORT_BY_DEFAULT];
+    sortKey = this.SORT_BY_DEFAULT;
 
     constructor(private organisationService: OrganisationService,
                 private router: Router,
                 private confirmationService: ConfirmationService,
                 private messageService: MessageService) {
+    }
+
+    ngOnChanges(): void {
+        this.sortOrganisations();
     }
 
     navigateToOrganisation(organisation: OrganisationResponse): void {
@@ -27,5 +38,28 @@ export class OrganisationDataViewComponent {
 
     getOrganisationPictureUrl(organisation: OrganisationResponse): string {
         return this.organisationService.getOrganisationPictureLink(organisation.id, organisation.picture);
+    }
+
+    onSortChange(event: any): void {
+        this.sortOrganisations();
+    }
+
+    sortOrganisations() {
+        if (this.sortKey === this.SORT_BY_RATING) {
+            this.sortedOrganisations = [...this.organisations];
+            this.sortedOrganisations.sort((a, b) => b.averageRating - a.averageRating);
+        } else {
+            this.sortedOrganisations = [...this.organisations];
+        }
+    }
+
+    filterByName(event: any) {
+        this.sortOrganisations();
+        const orgName = event.target.value.toLowerCase();
+        if (!orgName) {
+            return;
+        }
+        this.sortedOrganisations = this.sortedOrganisations
+            .filter(organisation => organisation.name.toLowerCase().includes(orgName));
     }
 }
